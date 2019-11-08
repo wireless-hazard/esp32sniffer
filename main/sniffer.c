a90ab9be5768f0729d7776df79e7fc44f615e83c
Magno Maia <magnocostamaia@gmail.com>
2019-11-07 23:58:10 -0300

    a

--- main/sniffer.c
+++ main/sniffer.c
@@ -1,10 +1,15 @@
-#include "freertos/FreeRTOS.h"
-#include "esp_wifi.h"
+#include <string.h>
 #include "esp_system.h"
-#include "esp_event.h"
-#include "esp_event_loop.h"
+#include "esp_log.h"
 #include "nvs_flash.h"
 #include "driver/gpio.h"
+#include "esp_wifi.h"
+#include "esp_event_loop.h"
+#include "esp_mesh.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "lwip/sockets.h"
 
 esp_err_t event_handler(void *ctx, system_event_t *event)
 {
